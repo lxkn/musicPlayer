e84547484fd0c315a219c9ac44aaa4766c607b31
Sebastian Druć <sed0180@gmail.com>
2017-01-14 12:25:52 +0100

    Singleton + próba wczytywania listy z xml

---
+++ Library.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace mPlayer
+{
+    /// <summary>
+    /// Interaction logic for Library.xaml
+    /// </summary>
+    public partial class Library : Window
+    {
+        private static Library singletonInstance;
+        private List <String> albumList;
+        
+        private Library()
+        {
+            /*String xml = "<albumList><object><album></album><author></author><year></year></object></albumList>";
+            XDocument doc = XDocument.Parse(xml);
+            albumList = doc.Root.Elements("object").Select(x => x.album).ToList();*/
+            InitializeComponent();
+        }
+        public static Library Instance
+        {
+            get
+            {
+                if(singletonInstance == null)
+                {
+                    singletonInstance = new Library();
+                }
+                return singletonInstance;
+            }
+        }
+    }
+}
